b738fb3afe3dea63fb7739838e268011e97109a8
Oleksandr <rifers@ukr.net>
2024-05-22 01:25:35 +0300

    moved cv file id to .env

--- app/config.py
+++ app/config.py
@@ -1,8 +1,13 @@
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 abs_path = Path(__file__).parent.resolve()
-cv_url = "https://drive.google.com/uc?export=download&id=17FfTBIJjN4vfJAICJ9NIeT6GUo590nYm"
+cv_id = os.getenv("GOOGLE_DRIVE_FILE_ID")
+cv_url = f"https://drive.google.com/uc?export=download&id={cv_id}"
 
 data_path = os.path.join(abs_path, "data")
 cv_path = os.path.join(data_path, "cv.pdf")
